5d37219f80598fa4804a428bd68bf355c4d9b2a1
anuj pal <=>
2024-07-17 16:07:05 +0530

    Added new changes

---
+++ src/app/users/page.tsx
@@ -0,0 +1,37 @@
+'use client';
+import React from 'react';
+import {
+  QueryClient,
+  QueryClientProvider,
+  useQuery,
+} from '@tanstack/react-query';
+
+import { User } from '@/types';
+import UserCard from '@/components/UserCard';
+
+const getUsers = async (): Promise<User[]> => {
+  const response = await fetch('https://jsonplaceholder.typicode.com/users');
+  return response.json();
+};
+
+const Users = () => {
+  const { isLoading, error, data } = useQuery({
+    queryKey: ['users'],
+    queryFn: getUsers,
+  });
+
+  if (isLoading) return <div>Loading users...</div>;
+
+  if (error) return <div>Error: {error.message}</div>;
+
+  return (
+    <>
+      <h3 className="mb-2 mt-5 text-center text-4xl font-extrabold">Users</h3>
+      <div className="grid grid-cols-1 gap-3 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+        {data?.map((user: User) => <UserCard key={user.id} user={user} />)}
+      </div>
+    </>
+  );
+};
+
+export default Users;
---
+++ src/components/UserCard.tsx
@@ -0,0 +1,37 @@
+import { User } from '@/types';
+import React from 'react';
+
+const UserCard = (props: { user: User }) => {
+  return (
+    <div key={props.user.id}>
+      <a
+        href="#"
+        className="mt-4 block max-w-sm rounded-lg bg-white p-6 shadow hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700"
+      >
+        <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+          {props.user.name}
+        </h5>
+        <p className="font-normal text-gray-700 dark:text-gray-400">
+          {props.user.website}
+        </p>
+        <p className="font-normal text-gray-700 dark:text-gray-400">
+          {props.user.company.catchPhrase}
+        </p>
+
+        <p className="font-normal text-gray-700 dark:text-gray-400">
+          {props.user.email}
+        </p>
+
+        <p className="font-normal text-gray-700 dark:text-gray-400">
+          {props.user.phone}
+        </p>
+
+        <p className="font-normal text-gray-700 dark:text-gray-400">
+          {props.user.username}
+        </p>
+      </a>
+    </div>
+  );
+};
+
+export default UserCard;
