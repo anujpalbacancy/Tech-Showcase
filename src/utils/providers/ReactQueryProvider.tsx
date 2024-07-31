34fc8e7753e86e017dfbde19c3f2f657181e453a
anuj pal <=>
2024-07-31 11:54:22 +0530

    minor changes

--- src/utils/providers/ReactQueryProvider.tsx
+++ src/utils/providers/ReactQueryProvider.tsx
@@ -4,6 +4,7 @@ import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
 import React, { useState } from 'react';
 
 const ReactQueryProvider = ({ children }: { children: React.ReactNode }) => {
+  // default cacheTime : 5min and staleTime:0
   const [queryClient] = useState(() => new QueryClient());
 
   return (
